9e9e8a8a5b340f07c340beaf491e3efca87c7ba4
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2018-01-07 20:21:06 +0100

    change

--- src/Repositories/FaqItemRepository.php
+++ src/Repositories/FaqItemRepository.php
@@ -42,6 +42,35 @@ class FaqItemRepository extends BaseRepository implements FaqItemRepositoryInter
         return $rules;
     }
 
+    public function create(array $attributes)
+    {
+        $attributes['shop_id'] = auth('hideyobackend')->user()->selected_shop_id;
+        $validator = Validator::make($attributes, $this->rules());
+
+        if ($validator->fails()) {
+            return $validator;
+        }
+
+        $attributes['modified_by_user_id'] = auth('hideyobackend')->user()->id;
+        $this->model->fill($attributes);
+        $this->model->save();
+        return $this->model;
+    }
+
+    public function updateById(array $attributes, $id)
+    {
+        $attributes['shop_id'] = auth('hideyobackend')->user()->selected_shop_id;
+        $validator = Validator::make($attributes, $this->rules($id));
+        if ($validator->fails()) {
+            return $validator;
+        }
+
+        $this->model = $this->find($id);
+        $attributes['modified_by_user_id'] = auth('hideyobackend')->user()->id;
+        return $this->updateEntity($attributes);   
+    }  
+    
+
     public function selectAllGroups()
     {
         return $this->modelFaqItemGroup->where('shop_id', '=', auth('hideyobackend')->user()->selected_shop_id)->get();
--- src/Repositories/NewsRepository.php
+++ src/Repositories/NewsRepository.php
@@ -77,6 +77,34 @@ class NewsRepository  extends BaseRepository implements NewsRepositoryInterface
         return $rules;
     }
 
+    public function create(array $attributes)
+    {
+        $attributes['shop_id'] = auth('hideyobackend')->user()->selected_shop_id;
+        $validator = Validator::make($attributes, $this->rules());
+
+        if ($validator->fails()) {
+            return $validator;
+        }
+
+        $attributes['modified_by_user_id'] = auth('hideyobackend')->user()->id;
+        $this->model->fill($attributes);
+        $this->model->save();
+        return $this->model;
+    }
+
+    public function updateById(array $attributes, $id)
+    {
+        $attributes['shop_id'] = auth('hideyobackend')->user()->selected_shop_id;
+        $validator = Validator::make($attributes, $this->rules($id));
+        if ($validator->fails()) {
+            return $validator;
+        }
+
+        $this->model = $this->find($id);
+        $attributes['modified_by_user_id'] = auth('hideyobackend')->user()->id;
+        return $this->updateEntity($attributes);   
+    }  
+
     public function createImage(array $attributes, $newsId)
     {
         $userId = auth('hideyobackend')->user()->id;
