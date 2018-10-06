bcc3b54fdd1d5279585016fcb306c345d9092fd0
Matthijs Neijenhuijs <matthijs@dutchbridge.nl>
2018-10-06 19:23:23 +0200

    mega clean up

--- src/Services/Content/ContentFacade.php
+++ src/Services/Content/ContentFacade.php
@@ -1,1 +1,21 @@
-ContentFacade.php
+<?php
+
+namespace Hideyo\Ecommerce\Framework\Services\Content;
+
+use \Illuminate\Support\Facades\Facade;
+
+/**
+ * Facade for user service
+ */
+class ContentFacade extends Facade
+{
+    /**
+     * Returning service name
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'Hideyo\Ecommerce\Framework\Services\Content\ContentService';
+    }
+}
--- src/Services/Content/ContentService.php
+++ src/Services/Content/ContentService.php
@@ -1,1 +1,76 @@
-ContentService.php
+<?php
+
+namespace Hideyo\Ecommerce\Framework\Services\Content;
+
+use Validator;
+use File;
+use Hideyo\Ecommerce\Framework\Services\Content\Entity\ContentRepository;
+use Hideyo\Ecommerce\Framework\Services\BaseService;
+ 
+class ContentService extends BaseService
+{
+	public function __construct(ContentRepository $taxRate)
+	{
+		$this->repo = $taxRate;
+	} 
+
+    /**
+     * The validation rules for the model.
+     *
+     * @param  integer  $taxRateId id attribute model    
+     * @return array
+     */
+    public function rules($taxRateId = false)
+    {
+        $rules = array(
+            'title' => 'required|between:2,65|unique_with:'.$this->repo->getModel()->getTable().', shop_id',
+            'rate'  => 'numeric|required'
+        );
+        
+        if($taxRateId) {
+            $rules['title'] =   $rules['title'].','.$taxRateId.' = id';
+        }
+
+        return $rules;
+    } 
+
+    public function create(array $attributes)
+    {
+        $attributes['shop_id'] = auth('hideyobackend')->user()->selected_shop_id;
+        $validator = Validator::make($attributes, $this->rules());
+
+        if ($validator->fails()) {
+            return $validator;
+        }
+
+        $attributes['modified_by_user_id'] = auth('hideyobackend')->user()->id;
+        $this->repo->getModel()->fill($attributes);
+        $this->repo->getModel()->save();
+        return $this->repo->getModel();
+    }
+
+    public function updateById(array $attributes, $id)
+    {
+        $attributes['shop_id'] = auth('hideyobackend')->user()->selected_shop_id;
+        $validator = Validator::make($attributes, $this->rules($id));
+        if ($validator->fails()) {
+            return $validator;
+        }
+
+        $model = $this->find($id);
+        $attributes['modified_by_user_id'] = auth('hideyobackend')->user()->id;
+
+        if (count($attributes) > 0) {
+            $model->fill($attributes);
+            $model->save();
+        }
+        return $model;  
+    } 
+
+    public function getGroupModel()
+    {
+        return $this->repo->getGroupModel();
+    }
+
+	
+}
